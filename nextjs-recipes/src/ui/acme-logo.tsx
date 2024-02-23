6e1c51ba789783a29f247e5cda1a02e2de38e88c
timytapilla <ajdnmkem@gmail.com>
2024-02-22 23:30:40 -0600

    new changes

--- nextjs-recipes/src/ui/acme-logo.tsx
+++ nextjs-recipes/src/ui/acme-logo.tsx
@@ -1,13 +1,16 @@
 import { ShoppingBagIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
     <div className={`flex flex-row items-center leading-none text-white`}>
       <ShoppingBagIcon className='h-12 w-12 rotate-[15deg]' />
-      <imgage
+      <Image
         src='/nextjs-recipes/src/app/cake.png'
         alt='Your Logo'
         className='h-16 md:h-20 ml-4'
+        width={150}
+        height={150}
       />
       <p className='text-[24px]'>Cake & Bakery</p>
     </div>
